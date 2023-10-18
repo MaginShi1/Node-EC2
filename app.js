e1694e30b38193f52e7e724e4dbbfda4121ddf0b
MaginShi <128712388+MaginShi1@users.noreply.github.com>
2023-10-18 15:40:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const http = require('http');
  
-const hostname = '34.255.2.129';
-const port = 80;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(function (req, res) {
+  res.write('** Hello world **'); 
+  res.end(); 
+}).listen(80);
